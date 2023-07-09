57111c6ee9519650d441f3e603b444f430113f51
marcosoliveira_dev <marcosoliveira253.mo@gmail.com>
2023-07-09 09:55:47 -0300

    correção na variavel hora

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ function carregar(){
     var today = document.getElementById('today');
 
     var date = new Date();
-    var hora =  20//date.getHours()
+    var hora =  date.getHours()
     var minutos = date.getMinutes()
     const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
     var dataFormatada = ((date.getDate() +  " - " + meses[(date.getMonth())] +  " - " + date.getFullYear())); 
     msg.innerHTML = `Agora são ${hora}:${minutos}hs`
     today.innerHTML = dataFormatada;
     
-    if(hora >= 0 && hora < 12) {
-       img.src = './img/manha.png'
-       document.body.style.background = `#e2cd9f`
+    if (hora > 18 && hora < 0) {
+       img.src = './img/noite.png'
+       document.body.style.background = `#e2cd9f
+       `
     } else if (hora >= 12 && hora <= 18) {
         img.src = './img/tarde.png'
         document.body.style.background = `#18ade7`
     } else {
-        img.src = './img/noite.png'
+        img.src = './img/manha.png'
         document.body.style.background = `#515154`
     }
 }
